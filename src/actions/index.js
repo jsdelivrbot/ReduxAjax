2d6bbd557c9e6afdf8b965b49630cd3a1ee2558f
Brian Sweeney <bsweeney930@gmail.com>
2017-04-14 14:21:44 -0400

    connected FetchWeather action to Searchbar component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const config = require('../config');
+const config = require('../../config');
 const API_KEY = config.API_KEY;
 const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER ='FETCH_WEATHER';
 
-export function fetchWeather(city) => {
+export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
