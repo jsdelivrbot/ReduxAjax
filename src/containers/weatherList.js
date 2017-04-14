d1e52aa9cec8301c4adff601cf082f4a9ede5da4
Brian Sweeney <bsweeney930@gmail.com>
2017-04-14 15:40:55 -0400

    cit names rendering on page

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { connect } from 'react-reduc';
+import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    return(
+      <tr>
+        <td>{cityData.city.name}</td>
+      </tr>
+    );
+  }
+
   render() {
     return (
       <table className="table table-hover">
@@ -14,7 +22,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
@@ -22,7 +30,7 @@ class WeatherList extends Component {
 }
 
 function mapStateToProps({weather}){
-  retrun { weather }//es6 ==={ weather: weather }
+  return { weather }//es6 ==={ weather: weather }
 }
 
 export default connect(mapStateToProps)(WeatherList)
